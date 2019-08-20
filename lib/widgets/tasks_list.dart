19139dc2ad5a9acb2bde99f61560281c04da072c
bob keri gabule <bobits@bobs-MacBook-Pro.local>
2019-08-20 10:30:50 +0800

    first upload

---
+++ lib/widgets/tasks_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'task_tile.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
+
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(builder: (context, taskData, child) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
+          return Dismissible(
+            key: Key(task.name),
+            onDismissed: (direction) {
+              taskData.deleteTask(task);
+            },
+            child: TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+            ),
+          );
+        },
+        itemCount: taskData.taskCount,
+      );
+    });
+  }
+}
